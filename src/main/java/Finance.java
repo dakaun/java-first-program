4470144c851ebfccb4bc44e7045337cdde500080
dakaun <daniela.kaun@gmx.de>
2021-06-25 10:25:18 +0200

    Finance - bug fix

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -10,34 +10,34 @@ public class Finance {
 	public final static String SAVINGS_CALCULATOR = "savingsCalculator";
 	public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
-	public final static Map<String, String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage: bestLoanRates",
+	public final static Map<String, String> commandsToUsage = Map.of(
+			BEST_LOAN_RATES, "usage: bestLoanRates",
 			SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
 			MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
 
 	private static boolean validateCommandArguments(String[] args) {
 		switch (args[0]) {
-		case "bestLoanRates":
+		case BEST_LOAN_RATES:
 			return args.length == 1;
-		case "savingsCalculator":
+		case SAVINGS_CALCULATOR:
 			return args.length == 3;
-		case "MortgageCalculator":
-			return args.length == 4;
-		default:
-			return false;
+		case MORTGAGE_CALCULATOR:
+			return args.length == 4;	
 		}
+		return false;
 	}
 
 	private static void executeCommand(String command, String[] arguments) {
 		switch (command) {
-		case "bestLoanRates":
+		case BEST_LOAN_RATES:
 			System.out.println("Finding best loan rates ...");
 			BestLoanRates.main(arguments);
 			return;
-		case "savingsCalculator":
+		case SAVINGS_CALCULATOR:
 			System.out.println("Finding your net savings ...");
 			SavingsCalculator.main(arguments);
 			return;
-		case "MortgageCalculator":
+		case MORTGAGE_CALCULATOR:
 			System.out.println("Finding your monthly payment ...");
 			MortgageCalculator.main(arguments);
 			return;
@@ -51,9 +51,10 @@ public class Finance {
 			System.out.println(command + ": command not found");
 			return;
 		}
+		
 		boolean isValidCommand = validateCommandArguments(args);
 		if (!isValidCommand) {
-			System.out.println(commandsToUsage.get(command));
+			System.out.println(commandsToUsage.get(args[0]));
 			return;
 		}
 
